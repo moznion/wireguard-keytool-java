7430b360ecc0254938caa097849eb9783d9d79d4
moznion <moznion@mail.moznion.net>
2021-05-17 01:07:34 +0900

    fmt
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- lib/src/test/java/net/moznion/wireguard/keytool/WireGuardKeyTest.java
+++ lib/src/test/java/net/moznion/wireguard/keytool/WireGuardKeyTest.java
@@ -1,11 +1,11 @@
 package net.moznion.wireguard.keytool;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WireGuardKeyTest {
 	@Test
