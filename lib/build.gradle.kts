aedb7b69f77e206b14d2ed5147b83f800cb33a20
moznion <moznion@mail.moznion.net>
2021-05-17 00:22:51 +0900

    Setup a method to publish the library to the Maven Central
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     `java-library`
     jacoco
+    `maven-publish`
+    signing
     id("com.diffplug.spotless") version "5.12.5"
 }
 
@@ -24,6 +26,8 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
 }
 
 tasks.build {
@@ -44,4 +48,53 @@ spotless {
         importOrder()
         target("**/*.java")
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            group = "net.moznion"
+            artifactId = "wireguard-keytool"
+            version = "0.0.1"
+            from(components["java"])
+            pom {
+                name.set("wireguard-keytool")
+                description.set("An utility library for WireGuard keys")
+                url.set("https://github.com/moznion/wireguard-keytool-java")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("moznion")
+                        name.set("Taiki Kawakami")
+                        email.set("moznion@mail.moznion.net")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/moznion/wireguard-keytool-java.git")
+                    developerConnection.set("scm:git:ssh://github.com/moznion/wireguard-keytool-java.git")
+                    url.set("https://github.com/moznion/wireguard-keytool-java")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl: String = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl: String = "https://oss.sonatype.org/content/repositories/snapshots"
+            setUrl(uri(if ((version as String).endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl))
+            credentials {
+                username = findProperty("sonatypeUsername") as String
+                password = findProperty("sonatypePassword") as String
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
